33413b00f8f2c6dd973d5f621fc44cf174eee898
전성모 <jeonsm22@daum.net>
2024-12-27 17:06:45 +0900

    nestjs study

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -12,8 +12,11 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
   @Post('/signup')
@@ -21,6 +24,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  // @UseInterceptors(new SerializeInterceptor(UserDto))
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
